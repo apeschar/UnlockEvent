71a5aa125fce38f93c1c3664cb205d0a197949c9
Albert Peschar <albert@peschar.net>
2021-05-19 11:01:48 +0300

    Initial Commit

---
+++ UnlockEvent3/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UnlockEvent3
+//
+//  Created by Albert on 5/19/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UnlockEvent3/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UnlockEvent3
+//
+//  Created by Albert on 5/19/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
